f651ce267267dab3fa038356b3dfe50c2b886bb9
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-09-07 12:58:08 +0530

    Create Leetcode 783. Minimum Distance Between BST Nodes.cpp

---
+++ Leetcode 783. Minimum Distance Between BST Nodes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mini =INT_MAX;
+    vector<int> temp;
+    void inorder(TreeNode* root){
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        temp.push_back(root->val);
+        inorder(root->right);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        int n=temp.size();
+        for(int i=0;i<n-1;i++){
+            mini =min(mini,(temp[i+1]-temp[i]));
+        }
+        return mini;
+    }
+};
