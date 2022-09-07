c34f4e42b93438158f684dcf222416ddf053acde
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-09-07 14:19:53 +0530

    Create leetcode 701. Insert into a Binary Search Tree.cpp

---
+++ leetcode 701. Insert into a Binary Search Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) 
+    {
+        if(root==NULL) 
+            return new TreeNode(val);
+        if(root->val<val)
+            root->right = insertIntoBST(root->right,val);
+        if(root->val>val) 
+            root->left = insertIntoBST(root->left,val);
+        return root;
+    }
+};
